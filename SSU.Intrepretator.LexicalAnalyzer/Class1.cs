a0de90e69f90a6a0b5f9ea05c48ede1dcb092645
121max1 <x2w3g4@gmail.com>
2021-10-02 19:01:16 +0400

    Добавьте файлы проекта.

---
+++ SSU.Intrepretator.LexicalAnalyzer/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SSU.Intrepretator.LexicalAnalyzer
+{
+    enum State { S, E, F, Ai, Ac, As, Bs, Cs, Ds, Gs }
+    enum LexType {Do, Until, Loop, Not, And, Or, Output, Rel, Ao, As }
+    public class LexixalAnylyser
+    {
+       public void LexAnalyzer(string text)
+        {
+            State state = State.S, prevState;
+            bool add;
+
+            int textIndex = 0;
+            while(state != State.E && state!= State.F)
+            {
+                prevState = state;
+                add = true;
+                char symbol = text[textIndex];
+                switch (state)
+                {
+                    case State.S:
+                        if (char.IsWhiteSpace(symbol)) state = State.S;
+                        else if (char.IsDigit(symbol)) state = State.Ac;
+                        else if (char.IsLetter(symbol)) state = State.Ai;
+                        else if (symbol == '<') state 
+
+                            break;
+                    case
+                }
+            }
+
+        }
+    }
+}
---
+++ SSU.Intrepretator.LexicalAnalyzer/State.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSU.Intrepretator.LexicalAnalyzer
+{
+    public class State
+    {
+
+    }
+}
