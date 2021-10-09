5f0de46812d209400c57011d045d017988d7f18e
121max1 <x2w3g4@gmail.com>
2021-10-09 18:58:17 +0400

    Pora domoy

---
+++ SSU.Intrepretator.SyntaxAnalyzer/Node.cs
@@ -0,0 +1,18 @@
+﻿using SSU.Intrepretator.LexicalAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSU.Intrepretator.SyntaxAnalyzer
+{
+    public class Node
+    {
+
+        public int Level { get; set; }
+        public Lex Token { get; set; }
+
+        
+
+        
+    }
+}
--- SSU.Intrepretator.SyntaxAnalyzer/SyntaxAnalyzer.cs
+++ SSU.Intrepretator.SyntaxAnalyzer/SyntaxAnalyzer.cs
@@ -1,5 +1,4 @@
 ﻿using SSU.Intrepretator.LexicalAnalyzer;
-using System;
 using System.Collections.Generic;
 
 namespace SSU.Intrepretator.SyntaxAnalyzer
@@ -7,15 +6,65 @@ namespace SSU.Intrepretator.SyntaxAnalyzer
     public class SyntaxAnalyzer
     {
         private readonly LexBuffer buffer;
+        private List<Lex> _tokens;
+
         public SyntaxAnalyzer(List<Lex> tokens)
         {
             buffer = new LexBuffer(tokens);
+            _tokens = tokens;
+        }
+        public void Run()
+        {
+            foreach(var token in _tokens)
+            {
+                
+            }
         }
 
-        private static void Operand()
+        private bool IsWhileStatement(int startIndex)
+        {
+            if(d)
+        }
+
+        private bool IsCondition()
         {
 
         }
+
+        private bool IsLogExpession()
+        {
+
+        }
+
+        private bool IsRelExpression()
+        {
+
+        }
+
+        private bool IsOperand()
+        {
+
+        }
+
+        private bool IsLogicalOperation()
+        {
+
+        }
+
+        private bool IsStatement()
+        {
+
+        }
+
+        bool IsArithmExpression()
+        {
+                
+        }
+
+
+
+
+
 
 
 
